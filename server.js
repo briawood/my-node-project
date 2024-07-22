ed201cf3541a27d92da9850fe9c735fd1aad3ca7
briawood <brian@briansmusic.com>
2024-07-21 22:19:46 -0600

    Fix mongo calls

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const express = require('express'); const app = express();
 const mongoose = require('mongoose'); const User = require('./models');
+const bodyParser = require('body-parser');
 mongoose.connect('mongodb://localhost:27017/myapp');
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:')); db.once('open', () => { console.log('Connected to database'); });
 
-    app.get('/', (req, res) => { res.send('Hello World!'); });
-app.post('/users', (req, res) => {
+app.get('/', (req, res) => { res.send('Hello World!'); });
+app.post('/users', bodyParser.json(), (req, res) => {
     const user = new User({ name: req.body.name, email: req.body.email, age: req.body.age });
-    user.save((err, user) => { if (err) return console.error(err); res.send(user); });
+//    user.save((err, user) => { if (err) return console.error(err); res.send(user); });
+    user.save()
+    .then(function (user) {
+        res.send(user);
+    })
+    .catch(function (err) {
+    console.log(err);
+    });
 });
 app.get("/users", function(req, res){
     User.find({})
